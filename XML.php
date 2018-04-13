51f3021b7dafc9896d930d5a6c79c38c6d5cba8c
Vinicius Alves <vinialves08@gmail.com>
2018-04-12 23:35:15 -0300

    Implementando outras funções

--- XML.php
+++ XML.php
@@ -6,18 +6,38 @@
 class XML
 {
 	public static function array_to_XML($data = array(),$xml = "")
-		{
-			foreach ($data as $keys => $values) {
-				if (is_array($values)) {
-					$xml .= "\r<".$keys.">";
-						$xml .= XML::array_to_XML($values);
-					$xml .= "\r</".$keys.">";
-				}else{
-					$xml .= "\r\t<".$keys.">".$values."</".$keys.">";
-				}
+	{
+		foreach ($data as $keys => $values) {
+			if (is_array($values)) {
+				$xml .= "\r<".$keys.">";
+					$xml .= XML::array_to_XML($values);
+				$xml .= "\r</".$keys.">";
+			}else{
+				$xml .= "\r\t<".$keys.">".$values."</".$keys.">";
 			}
-			return $xml;
-		}	
+		}
+		return $xml;
+	}	
+	public static function XML_to_array($arquivo = ""){
+		if (file_exists($arquivo.'.xml')) {
+ 		   return simplexml_load_file($arquivo.'.xml');
+		} else {
+    		return 'Falha ao abrir test.xml.';
+		}
+	}
+	public static function generateCsv($arquivo, $data, $delimiter = ',', $enclosure = '"') {
+       $handle = fopen($arquivo.".csv", 'a+');
+       $contents = "";
+       foreach ($data as $line) {
+               fputcsv($handle, $line, $delimiter, $enclosure);
+       }
+       rewind($handle);
+       while (!feof($handle)) {
+               $contents .= fread($handle, 8192);
+       }
+       fclose($handle);
+       return $contents;
+}
 }
 
 
--- teste.php
+++ teste.php
@@ -6,20 +6,20 @@
 		'sobrenome' => "Alves",
 		'email' => "vinialves08@gmail.com",
 		'dtnasc' => "21/03/1993",
-		'escolaridade'=> "Superior Completo",
-		'sexo'=> "Masculino",
-		'endereco' => array(
-			'Avenida' => "N003",
-			'Bairro' => "Anápolis City"
-		)
+		'escolaridade'=> "Mestrando",
+		'sexo'=> "Masculino"
 	));
 	$xml = "<?xml version='1.0' encoding='ISO-8859-1'?>";
-	$pessoa_xml = XML::array_to_XML($pessoa, $xml);
-	
 
+	/*$pessoa_xml = XML::array_to_XML($pessoa, $xml);
 	$nome_arquivo = "cadastro2.xml";
 	// Escreve o arquivo
 	$fp = fopen($nome_arquivo, 'w+');
 	fwrite($fp, $pessoa_xml);
-	fclose($fp);
+	fclose($fp);*/
+
+	/*$pessoa_array = XML::XML_to_array("cadastro");
+	print_r($pessoa_array);*/
+
+	XML::generateCsv("usuario", $pessoa);
  ?>
